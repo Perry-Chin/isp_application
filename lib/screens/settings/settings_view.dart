cb12e55a98e5d7287ea9aaf45a02fef17a5fbc3b
IaAmPerson <ylt012321@gmail.com>
2024-05-23 10:57:58 +0800

    settings frontend update

--- lib/screens/settings/settings_view.dart
+++ lib/screens/settings/settings_view.dart
@@ -17,97 +17,103 @@ class SettingsPage extends GetView<SettingsController> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: _buildAppBar(),
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            
-            SizedBox(
-              height: 75,
-              child: InkWell(
-                onTap: () {
-                  // Handle the tap event for the blue box
-                  print("box1 tapped");
-                },
-                child: Container(
-                  margin: EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    //color: Colors.blue,
-                    borderRadius: BorderRadius.circular(16.0), // Rounded edges
-                    border: Border.all(
-                      color: Colors.black, // Border color
-                      width: 2.0, // Border width
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Edit Profile',
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(
-              height: 75,
-              child: InkWell(
-                onTap: () {
-                  // Handle the tap event for the blue box
-                  print("box2 tapped");
-                },
-                child: Container(
-                  margin: EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    //color: Colors.blue,
-                    borderRadius: BorderRadius.circular(16.0), // Rounded edges
-                    border: Border.all(
-                      color: Colors.black, // Border color
-                      width: 2.0, // Border width
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Any Problems? Contact Us!',
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(
-              height: 75,
-              child: InkWell(
-                onTap: () {
-                  // Handle the tap event for the blue box
-                  print("box3 tapped");
-                },
-                child: Container(
-                  margin: EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    //color: Colors.blue,
-                    borderRadius: BorderRadius.circular(16.0), // Rounded edges
-                    border: Border.all(
-                      color: Colors.black, // Border color
-                      width: 2.0, // Border width
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'View Payment Details',
-                      style: TextStyle(color: Colors.black, fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-          ],
-        ),
-      ),
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: const SetSettingsPage(),
     );
   }
 }
+
+class SetSettingsPage extends StatelessWidget {
+  const SetSettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        SizedBox(
+          height: 75,
+          child: InkWell(
+            onTap: () {
+              // Handle the tap event for the blue box
+              print("box1 tapped");
+            },
+            child: Container(
+              margin: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                //color: Colors.blue,
+                borderRadius: BorderRadius.circular(16.0), // Rounded edges
+                border: Border.all(
+                  color: Colors.black, // Border color
+                  width: 2.0, // Border width
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'Edit Profile',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        SizedBox(
+          height: 75,
+          child: InkWell(
+            onTap: () {
+              // Handle the tap event for the blue box
+              print("box2 tapped");
+            },
+            child: Container(
+              margin: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                //color: Colors.blue,
+                borderRadius: BorderRadius.circular(16.0), // Rounded edges
+                border: Border.all(
+                  color: Colors.black, // Border color
+                  width: 2.0, // Border width
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'Any Problems? Contact Us!',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        SizedBox(
+          height: 75,
+          child: InkWell(
+            onTap: () {
+              // Handle the tap event for the blue box
+              print("box3 tapped");
+            },
+            child: Container(
+              margin: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                //color: Colors.blue,
+                borderRadius: BorderRadius.circular(16.0), // Rounded edges
+                border: Border.all(
+                  color: Colors.black, // Border color
+                  width: 2.0, // Border width
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'View Payment Details',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ),
+
+      ],
+    );
+  }
+}
+

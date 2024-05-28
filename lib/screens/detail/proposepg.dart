0aacbc090c2d1d762e297d7ef398f95fb68477b0
shermainejx <xuanj0555@gmail.com>
2024-05-29 01:16:14 +0800

    nil

--- lib/screens/detail/proposepg.dart
+++ lib/screens/detail/proposepg.dart
@@ -1,5 +1,8 @@
-<<<<<<< HEAD
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+// import '../../common/data/service.dart';
+// import '../../common/data/user.dart';
 
 Future<void> proposeNewPage(BuildContext context) async {
   showModalBottomSheet(
@@ -8,58 +11,74 @@ Future<void> proposeNewPage(BuildContext context) async {
       return SafeArea(
         child: Wrap(
           children: [
-            // Add your content here
-            ListTile(
-              leading: Icon(Icons.access_time),
-              title: Text('Propose a new time'),
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      "Propose Time",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    ),
+                  ),
+                  const Text(
+                    "Once submitted, BuzzBuddy will share your proposed time with the Requester for their confirmation.",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Original Date & Time",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(16.0), // Set padding for the container
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0), // Set border radius
+                      color: Colors.grey[200], // Set background color
+                    ),
+                    child: const Text(
+                      'Your original date and time here',
+                      style: TextStyle(
+                        fontSize: 16.0, // Set font size
+                        color: Colors.black, // Set text color
+                      ),
+                    ),
+                  ),
+                 Text(
+                  "Propose Time",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                  ),
+                 ),
+                 Container(
+                   padding: const EdgeInsets.all(16.0), // Set padding for the container
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0), // Set border radius
+                      color: Colors.grey[200], // Set background color
+                    ),
+                    child: const Text(
+                      'Start',
+                      style: TextStyle(
+                        fontSize: 16.0, // Set font size
+                        color: Colors.black, // Set text color
+                      ),
+                    ),
+                 )
+                ],
+              ),
             ),
-            Text(
-              "Once submitted, BuzzBuddy will share your proposed time with the Requester for their confirmation.",
-            )
           ],
         ),
       );
     },
   );
 }
-=======
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:get/get.dart';
-
-// import '../../common/data/data.dart';
-// import '../../common/values/values.dart';
-// import '../../common/widgets/widgets.dart';
-// import 'detail_index.dart';
-
-
-// Future<void> proposeNewPage(BuildContext context) async {
-//   showModalBottomSheet(
-//     context: context, 
-//     builder: (BuildContext bc) {
-//       return SafeArea(
-//         child: Wrap(
-//           children: [
-//             ListTile(
-//               leading: const Icon(Icons.photo_library),
-//               title: const Text("Gallery"),
-//               onTap: () {
-//                 imgFromGallery(onImageSelected);
-//                 Get.back();
-//               },
-//             ),
-//             ListTile(
-//               leading: const Icon(Icons.photo_camera),
-//               title: const Text("Camera"),
-//               onTap: () {
-//                 imgFromCamera(onImageSelected);
-//                 Get.back();
-//               },
-//             )
-//           ],
-//         )
-//       );
-//     }
-//   );
-// }
->>>>>>> 78fc015db7f261357f60470f135dd6edc384b973

db9a8441aba7941454383ee04f93408d87a67f37
Perry-Chin <149997613+Perry-Chin@users.noreply.github.com>
2024-05-30 11:31:53 +0800

    Update message_controller.dart

--- lib/screens/message/message_controller.dart
+++ lib/screens/message/message_controller.dart
@@ -3,7 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
-import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/rxdart.dart' as rxdart;
 
 import '../../common/data/data.dart';
 import '../../common/storage/storage.dart';
@@ -17,79 +17,95 @@ class MessageController extends GetxController {
   final MessageState state = MessageState();
   var listener;
 
-  final RefreshController refreshController = RefreshController (
+  final RefreshController refreshController = RefreshController(
     initialRefresh: true
   );
 
-  //The dependency requires this 2 functions to work
+  //The dependency requires these 2 functions to work
   void onRefresh() {
-    asyncLoadAllData().then((_){
+    asyncLoadAllData().then((_) {
       refreshController.refreshCompleted(resetFooterState: true);
-    }).catchError((_){
+    }).catchError((_) {
       refreshController.refreshFailed();
     });
   }
- 
+
   void onLoading() {
-    asyncLoadAllData().then((_){
+    asyncLoadAllData().then((_) {
       refreshController.loadComplete();
-    }).catchError((_){
+    }).catchError((_) {
       refreshController.loadFailed();
     });
   }
-  //
 
-  
-// Stream to handle fetching service data
-  Stream<List<QueryDocumentSnapshot<ServiceData>>> getServiceStream(String token) {
-    return db
-        .collection('service')
-        .where('requester_uid', isNotEqualTo: token)
-        .where('status', isEqualTo: 'Requested')
-        .withConverter<ServiceData>(
-          fromFirestore: ServiceData.fromFirestore,
-          toFirestore: (ServiceData serviceData, _) => serviceData.toFirestore(),
-        )
-        .snapshots()
-        .map((snapshot) => snapshot.docs);
+  // Stream to handle fetching message data
+  Stream<List<QueryDocumentSnapshot<Msg>>> getMessageStream(String token) {
+    var fromStream = db
+      .collection('message')
+      .where('from_uid', isEqualTo: token)
+      .withConverter<Msg>(
+        fromFirestore: Msg.fromFirestore,
+        toFirestore: (Msg msg, _) => msg.toFirestore(),
+      )
+      .snapshots()
+      .map((snapshot) => snapshot.docs);
+
+    var toStream = db
+      .collection('message')
+      .where('to_uid', isEqualTo: token)
+      .withConverter<Msg>(
+        fromFirestore: Msg.fromFirestore,
+        toFirestore: (Msg msg, _) => msg.toFirestore(),
+      )
+      .snapshots()
+      .map((snapshot) => snapshot.docs);
+
+    return rxdart.Rx.combineLatest2(fromStream, toStream, (List<QueryDocumentSnapshot<Msg>> fromMessages, List<QueryDocumentSnapshot<Msg>> toMessages) {
+      return [...fromMessages, ...toMessages];
+    });
   }
 
   // Stream to handle fetching user data
   Stream<List<DocumentSnapshot<UserData>>> getUserStream(List<String> userIds) {
     return db
-        .collection('users')
-        .where(FieldPath.documentId, whereIn: userIds)
-        .withConverter<UserData>(
-          fromFirestore: UserData.fromFirestore,
-          toFirestore: (UserData userData, _) => userData.toFirestore(),
-        )
-        .snapshots()
-        .map((snapshot) => snapshot.docs);
+      .collection('users')
+      .where(FieldPath.documentId, whereIn: userIds)
+      .withConverter<UserData>(
+        fromFirestore: UserData.fromFirestore,
+        toFirestore: (UserData userData, _) => userData.toFirestore(),
+      )
+      .snapshots()
+      .map((snapshot) => snapshot.docs);
   }
 
-  // Combine the streams to get user data for each service item
+  // Combine the streams to get user data for each message item
   Stream<Map<String, UserData?>> getCombinedStream(String token) {
-    return getServiceStream(token).switchMap((serviceDocs) {
-      List<String> userIds = serviceDocs.map((doc) => doc.data().reqUserid!).toList();
+    return getMessageStream(token).switchMap((messageDocs) {
+      List<String> userIds = messageDocs.expand((doc) {
+        return [doc.data().fromUserid, doc.data().toUserid];
+      }).whereType<String>().toSet().toList();
 
       if (userIds.isEmpty) {
         return Stream.value({});
       }
 
       return getUserStream(userIds).map((userDocs) {
-        var userDataMap = Map.fromEntries(userDocs.map((doc) => MapEntry(doc.id, doc.data())));
+        var userDataMap = Map.fromEntries(userDocs.map((doc) {
+          print("Fetched User ID: ${doc.id}, Data: ${doc.data()}");
+          return MapEntry(doc.id, doc.data());
+        }));
         state.userDataMap.assignAll(userDataMap); // Update userDataMap in state
         return userDataMap;
       });
     });
   }
-  
+
   // Stream to handle data fetching
   Stream<Map<String, UserData?>> get combinedStream async* {
     await asyncLoadAllData();
     yield* getCombinedStream(UserStore.to.token);
   }
-  
+
   asyncLoadAllData() async {
     var fromMessages = await db.collection("message").withConverter(
       fromFirestore: Msg.fromFirestore, 
@@ -106,7 +122,7 @@ class MessageController extends GetxController {
 
     if(fromMessages.docs.isNotEmpty) {
       state.msgList.assignAll(fromMessages.docs);
-    } 
+    }
 
     if(toMessages.docs.isNotEmpty) {
       state.msgList.assignAll(toMessages.docs);
